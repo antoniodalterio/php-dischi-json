17df9e34ac217ab2a97033c64ca8aa24185623da
antoniodalterio <antoniodalterio2227@gmail.com>
2024-01-11 17:25:22 +0100

    feat: call get with axios

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: 'hello',
+      albums: [],
     };
+  },
+  methods: {
+    getAlbum() {
+      axios.get('server.php').then((response) => {
+        this.albums = response.data;
+        console.log(this.albums);
+      });
+    },
+  },
+  created() {
+    this.getAlbum();
   },
 }).mount('#app');
