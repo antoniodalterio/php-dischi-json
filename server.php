c2c2952cade4563fef91ff8159cb06068424de4b
antoniodalterio <antoniodalterio2227@gmail.com>
2024-01-12 01:05:11 +0100

    feat: style

--- server.php
+++ server.php
@@ -3,42 +3,42 @@ $album = [
   [
     'name' => 'New Jersey',
     'release' => '1988',
-    'directed' => 'Bon Jovi',
+    'author' => 'Bon Jovi',
     'src' => 'https://upload.wikimedia.org/wikipedia/en/thumb/2/2c/Bon_Jovi_New_Jersey.jpg/220px-Bon_Jovi_New_Jersey.jpg'
 
   ],
   [
     'name' => 'Live at Wembley 86',
     'release' => '1982',
-    'directed' => 'Quenn',
+    'author' => 'Queen',
     'src' => 'https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Queen_Live_At_Wembley_%2786.png/220px-Queen_Live_At_Wembley_%2786.png'
 
   ],
   [
     'name' => "Ten's Summoner's Tales",
     'release' => '1993',
-    'directed' => 'Stind',
+    'author' => 'Sting',
     'src' => 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/Sting_-_Ten_Summoner%27s_Tales.png/220px-Sting_-_Ten_Summoner%27s_Tales.png'
 
   ],
   [
     'name' => 'Steve Gadd band',
     'release' => '2018',
-    'directed' => 'Steve Gadd band',
+    'author' => 'Steve Gadd band',
     'src' => 'https://i.scdn.co/image/ab67616d00001e0203cea6838961201fb58bea4f'
 
   ],
   [
     'name' => 'Brave new World',
     'release' => '2000',
-    'directed' => 'Iron Maiden',
+    'author' => 'Iron Maiden',
     'src' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg'
 
   ],
   [
     'name' => 'One more car, one more',
     'release' => '2002',
-    'directed' => 'Eric Clapton',
+    'author' => 'Eric Clapton',
     'src' => 'https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Eric_Clapton_OMCOMR.jpg/220px-Eric_Clapton_OMCOMR.jpg'
 
   ],
